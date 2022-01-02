0f7797632dc9fe487987b4625f305b1661c40bfe
delcidj22 <jdelcdid23@gmail.com>
2022-01-02 15:47:00 -0800

    get first test to pass

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -1,1 +1,19 @@
 import Years from './../src/js/years.js';
+
+
+describe('Years', ()=> {
+  let year;
+  beforeEach(()=> {
+    year = new Years(25,90);
+  });
+
+  test('should create a Years object with age and lifeExpectancy', () => {
+    expect(year.age).toEqual(25);
+    expect(year.lifeExpectancy).toEqual(90);
+  });
+
+  test('should create users age in mercury years', () => {
+    expect(year.mercuryAge(year.age)).toEqual(104.1667);
+  });
+  
+});
