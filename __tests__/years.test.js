5aa27fbff59f31871749e4d64097b0ee1417c7db
delcidj22 <jdelcdid23@gmail.com>
2022-01-02 16:17:49 -0800

    decided to add users age on earth

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -6,10 +6,15 @@ describe('Years', ()=> {
   beforeEach(()=> {
     year = new Years(25,90);
   });
+  
 
   test('should create a Years object with age and lifeExpectancy', () => {
     expect(year.age).toEqual(25);
     expect(year.lifeExpectancy).toEqual(90);
+  });
+
+  test('should return users age here on earth', () => {
+    expect(year.earthAge(year.age)).toEqual(25);
   });
 
   test('should create users age in mercury years', () => {
