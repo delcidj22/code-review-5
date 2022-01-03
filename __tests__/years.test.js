04c7164b53f20abe689981c6544fde47d12fc3ea
delcidj22 <jdelcdid23@gmail.com>
2022-01-02 16:10:37 -0800

    get fifth test to pass

--- __tests__/years.test.js
+++ __tests__/years.test.js
@@ -19,5 +19,13 @@ describe('Years', ()=> {
   test('should create a users age in venus years', () => {
     expect(year.venusAge(year.age)).toEqual(40.32258064516129);
   });
+
+  test('should create a users age in mars years', () => {
+    expect(year.marsAge(year.age)).toEqual(13.297872340425533);
+  });
+
+  test('should create a users age in jupiter years', () => {
+    expect(year.jupiterAge(year.age)).toEqual(2.1079258010118043);
+  });
   
 });
